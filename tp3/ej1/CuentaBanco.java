0b388bbb07028869a5977d79d9d6eacd910a35c9
joa <J0A@J0A>
2020-09-18 16:06:32 -0300

    tp3 puesta en común

--- tp3/ej1/CuentaBanco.java
+++ tp3/ej1/CuentaBanco.java
@@ -16,12 +16,27 @@ public class CuentaBanco {
     public CuentaBanco() {
     }
 
-    public int getBalance() {
+    public synchronized int getBalance() {
         return balance;
     }
 
+    //agrego synchronized al método hacerRetiro para que accedan un hilo a la vez 
+    public synchronized void HacerRetiro(int cantidad) throws InterruptedException {
+        if (this.getBalance() >= cantidad) {
+            System.out.println(Thread.currentThread().getName() + " está realizando un retiro de: " + cantidad + ".");
+            Thread.sleep(1000);
+            this.retiroBancario(cantidad);
+            System.out.println(Thread.currentThread().getName() + ": Retiro realizado.");
+            System.out.println(Thread.currentThread().getName() + ": Los fondos son de: " + this.getBalance());
+        } else {
+            System.out.println("No hay suficiente dinero en la cuenta para realizar el retiro Sr. " + Thread.currentThread().getName());
+            System.out.println("Su saldo actual es de " + this.getBalance());
+            Thread.sleep(1000);
+        }
+    }
+
     //si sólo sincronizo verificarCuenta, el método retiroBancario me queda "libre" y puede generar problemas
-    public synchronized void retiroBancario(int retiro) {
+    private void retiroBancario(int retiro) {
         balance = balance - retiro;
     }
 }
--- tp3/ej1/VerificarCuenta.java
+++ tp3/ej1/VerificarCuenta.java
@@ -11,27 +11,12 @@ import java.util.logging.*;
  */
 public class VerificarCuenta implements Runnable {
 
-    private CuentaBanco cb = new CuentaBanco();
-
-//agrego synchronized al método hacerRetiro para que accedan un hilo a la vez   
-    private synchronized void HacerRetiro(int cantidad) throws InterruptedException {
-        if (cb.getBalance() >= cantidad) {
-            System.out.println(Thread.currentThread().getName() + " está realizando un retiro de: " + cantidad + ".");
-            Thread.sleep(1000);
-            cb.retiroBancario(cantidad);
-            System.out.println(Thread.currentThread().getName() + ": Retiro realizado.");
-            System.out.println(Thread.currentThread().getName() + ": Los fondos son de: " + cb.getBalance());
-        } else {
-            System.out.println("No hay suficiente dinero en la cuenta para realizar el retiro Sr. " + Thread.currentThread().getName());
-            System.out.println("Su saldo actual es de " + cb.getBalance());
-            Thread.sleep(1000);
-        }
-    }
+    private CuentaBanco cb = new CuentaBanco();  
 
     public void run() {
         for (int i = 0; i <= 3; i++) {
             try {
-                this.HacerRetiro(10);
+                cb.HacerRetiro(10);
                 if (cb.getBalance() < 0) {
                     System.out.println("La cuenta está sobregirada.");
                 }
